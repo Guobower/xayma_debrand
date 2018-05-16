a41e507e71f2d499d1f7e33fc603f5e5f535ced8
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-16 17:37:12 +0000

    Suppression des fichiers inutiles

--- __manifest__.py
+++ __manifest__.py
@@ -1,38 +1,22 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Xayma Branding",
-
     'summary': """
         Enlève sur l'interface utilisateur, les références à Odoo""",
-
     'description': """
         Enlève les références à Odoo sur l'interface utilisateur, aux emplacements suivants :
         - Footer
         - Login Page
     """,
-
     'author': "Elhadji Malang DIEDHIOU",
     'website': "http://www.supermalang.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Usability',
+    'version': '11.0.1.0',
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
         'views/xayma.debrand.footer.template.xml',
         'views/xayma.debrand.header.xml',
         'views/xayma.debrand.loginpage.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'demo': [],
 }
